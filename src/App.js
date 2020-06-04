a76fa483f318d01449b48ffd6a04f396891a014f
Maciej Mazurek <maciej.mazurek.prv@gmail.com>
2020-06-04 09:41:17 +0200

    Create basic header component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
+import Header from "./components/Header";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello there</h1>
+    <div>
+      <Header />
     </div>
   );
 }
