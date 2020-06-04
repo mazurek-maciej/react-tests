d8ae30c010df0991524a0a70b91d9b3a7353160f
Maciej Mazurek <maciej.mazurek.prv@gmail.com>
2020-06-04 09:10:54 +0200

    Add node-sass and use as main styling

---
+++ src/components/Header/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./header.scss";
+
+const Header = () => {
+  return (
+    <header>
+      <p className="header-logo">IT Diary</p>
+    </header>
+  );
+};
+
+export default Header;
