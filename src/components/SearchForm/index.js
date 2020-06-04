6c285ab509b7fc1440586a548c8f8cfb7dcf57a2
Maciej Mazurek <maciej.mazurek.prv@gmail.com>
2020-06-04 17:20:54 +0200

    Create searchForm component

---
+++ src/components/SearchForm/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default () => {
+  return <form data-testid="form"></form>;
+};
---
+++ src/components/SearchForm/searchForm.test.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import SearchForm from "./index";
+
+describe("SearchForm", () => {
+  render(<SearchForm />);
+});
